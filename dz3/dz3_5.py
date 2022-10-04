5356d464fc141d167e3bb8cc910425371ebed5a2
Sasha_Nikonorov <i_sasha_n@mail.ru>
2022-10-04 23:49:23 +0300

    rename function

--- dz3/dz3_5.py
+++ dz3/dz3_5.py
@@ -5,16 +5,16 @@
 # - для k = 8 список будет выглядеть так:
 # [-21, 13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21][Негафибоначчи]
 
-def fibon(n):
+def negfibon(n):
     if n < 0:
-        return (-1) ** (-n + 1) * fibon(-n)
+        return (-1) ** (-n + 1) * negfibon(-n)
     if n in (1, 2):
         return 1
-    return fibon(n - 1) + fibon(n - 2)
+    return negfibon(n - 1) + negfibon(n - 2)
 
 
-fibneg = []
+lst = []
 num = int(input('Введите число '))
 for i in range(-num, num + 1):
-    fibneg.append(fibon(i))
-print(fibneg)
+    lst.append(negfibon(i))
+print(lst)
