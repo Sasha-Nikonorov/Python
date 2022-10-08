c8058b6076b9bb65ebf06558f1ec8e60ba107662
Sasha_Nikonorov <i_sasha_n@mail.ru>
2022-10-08 20:01:06 +0300

    forth dz

---
+++ dz4/dz4_3.py
@@ -0,0 +1,15 @@
+# Задайте последовательность чисел.
+# Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
+import random
+
+lst1 = []
+number = int(input('Введите число: '))
+for i in range(number):
+    new_element = random.randrange(1, number)
+    lst1.append(new_element)
+print(lst1)
+lst2 = []
+for i in lst1:
+    if lst1.count(i) < 2:
+        lst2.append(i)
+print(lst2)
